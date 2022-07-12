662105ee5b05b90bb06a4fd7870456f1d477aa32
Bruno Gouveia <78083006+brunovcg@users.noreply.github.com>
2022-07-11 23:46:46 -0300

    initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <>
+      <h1>Welcome to React TypeScript Webpack Bruno Gouveia</h1>
+      You are on {process.env.name} env
+      <button>xxx</button>
+    </>
+  )
+}
